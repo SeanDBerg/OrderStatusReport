9bb8955acba6b5c7d3ffe7d6abf5a8749af1daf2
Sean <143645128+SeanDBerg@users.noreply.github.com>
2025-07-07 16:11:33 +0000

    vite fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   root: __dirname,
-  base: './',
+  base: '/OrderStatusReport/',
   plugins: [react()],
   build: {
     outDir: "../docs",      // if you chose one-stage flow
